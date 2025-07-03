a4aeb18bec67a12f4da7b8c94f4f63927141a154
CaptainCool24 <paarthmhatre08@gmail.com>
2025-07-03 14:43:16 +0530

    fourth commit

--- create_tables.sql
+++ create_tables.sql
@@ -1,11 +1,13 @@
 CREATE DATABASE IF NOT EXISTS demo;
 USE demo;
+
 CREATE TABLE students ( 
-id INT PRIMARY KEY AUTO_INCREMENT, 
-name VARCHAR(100),
-marks INT
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    name VARCHAR(100),
+    marks INT
 );
+
 INSERT INTO students (name, marks) VALUES
-(Pranav, 85);
-(Pratham, 90);
-(Aditya, 76);
+('Pranav', 85),
+('Pratham', 90),
+('Aditya', 76);
